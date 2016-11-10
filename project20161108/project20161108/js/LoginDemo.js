1517bcc038169aa6437d30290bee958fcc626df9
yuhaihang <365738593@qq.com>
2016-11-10 18:29:07 +0800

    20161108登录网站

---
+++ project20161108/project20161108/js/LoginDemo.js
@@ -0,0 +1,36 @@
+﻿
+function ShowLoginBox() {
+    $("#txtUserName").val("");
+    $("#txtPwd").val("");
+    layer.open({
+        type: 1,
+        title: "登录",
+        area: ["393px", "260px"],
+        content: $("#dloginbox")
+    });
+}
+
+function Login() {
+    var username = $.trim($("#txtUserName").val());
+    var pwd = $.trim($("#txtPwd").val());
+    if (username == "" || pwd == "") {
+        layer.msg("用户名或密码不能为空", {
+            time: 1000,
+            icon: 5
+        })
+    }
+    if (username == "admin" && pwd == "admin") {
+        layer.msg("登录成功", {
+            time: 1000,
+            icon: 6
+        }, function () {
+            layer.open({
+                type: 2,
+                area: ['700px', '530px'],
+                fixed: false,
+                maxmin: true,
+                content: 'http://www.baidu.com'
+            });
+        });
+    }
+}
