12956e252bdaadca4347ca0ba98138d4432a07f2
yuhaihang <365738593@qq.com>
2016-11-24 21:27:48 +0800

    Layer

---
+++ project20160928/project20160928/project20160928/js/index.js
@@ -0,0 +1,35 @@
+﻿function ShowLoginBox() {
+    layer.open({
+        type: 1,//弹出一个页面层
+        title: "登录",
+        area: ["393px", "300px"],
+        content: $("#dloginbox")
+    });
+}
+
+function Login() {
+    var username = $.trim($("#txtUserName").val());
+    var pwd = $.trim($("#txtPwd").val());
+    if (username == "" || pwd == "") {
+        layer.msg("用户名或密码不能为空", {
+            icon: 5
+        });
+    }
+    else {
+        $.post("/Handler.ashx", { "username": username, "pwd": pwd }, function (data) {
+            if (data == "ok") {
+                layer.msg("登录成功", {
+                    icon: 6
+                }, function () {
+                    layer.closeAll();
+                    localStorage.href = "http://www.baidu.com";
+                });
+            }
+            else {
+                layer.msg("用户名或密码错误", {
+                    icon: 5
+                });
+            }
+        });
+    }
+}
